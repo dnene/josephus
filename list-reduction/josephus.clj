42cc09cc88a14ab7d6cd9d55f626a464fdb04836
Scott Jaderholm <jaderholm@gmail.com>
2011-08-23 11:58:27 +0800

    more type hints, speeds things up maybe ~5%

--- list-reduction/josephus.clj
+++ list-reduction/josephus.clj
@@ -1,28 +1,31 @@
-(defn shout [^long counter ^long nth [cnt coll]]
+(defn shout ^long [^long counter ^long nth ^long cnt coll]
   (if (= 1 cnt)
     (first coll)
-    (shout (rem (+ counter cnt) nth)
-           nth
-           (loop [index 0
-                  new-count 0
-                  remaining coll
-                  result (transient [])]
-             (if (seq remaining)
-               (if-not (zero? (rem (+ index counter) nth))
-                 (recur (inc index)
-                        (inc new-count)
-                        (rest remaining)
-                        (conj! result (first remaining)))
-                 (recur (inc index)
-                        new-count
-                        (rest remaining)
-                        result))
-               [new-count (persistent! result)])))))
+    (let [[new-count result]
+          (loop [index 0
+                 new-count 0
+                 remaining coll
+                 result (transient [])]
+            (if (seq remaining)
+              (if-not (zero? (rem (+ index counter) nth))
+                (recur (inc index)
+                       (inc new-count)
+                       (rest remaining)
+                       (conj! result (first remaining)))
+                (recur (inc index)
+                       new-count
+                       (rest remaining)
+                       result))
+              [new-count (persistent! result)]))]
+      (recur (rem (+ counter cnt) nth)
+             nth
+             (long new-count)
+             result))))
 
-(defn josephus [people nth]
-  (shout 0 nth [people (into [] (range 1 (inc people)))]))
+(defn josephus ^long [^long people ^long nth]
+  (shout 0 nth people (into [] (range 1 (inc people)))))
 
-(defn run-iterations [iterations times]
+(defn run-iterations [^long iterations ^long times]
   (dotimes [_ times]
     (let [start (System/nanoTime)]
         (dotimes[_ iterations] 
