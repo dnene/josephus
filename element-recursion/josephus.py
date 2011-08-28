6765b9272f3576558a12b5da28aa264d577d081f
Dhananjay Nene <dhananjay.nene@gmail.com>
2011-08-29 01:49:15 +0530

    Added contributions by isaac for python

--- element-recursion/josephus.py
+++ element-recursion/josephus.py
@@ -1,30 +1,57 @@
-from collections import deque
-def find(chainlength = 40, kill = 3):
-    return findlast(deque(range(1,chainlength+1)),3,1)
+"""
+$ python josephus.py
+26
+element recursion
+50.3187770844
+50.3241479397
+50.3240659237
+50.3192489147
+50.3268041611
+50.3255558014
+50.2993578911
+50.2927269936
+50.2895271778
+50.2902379036
+"""
 
-def findlast(chain,nth,counter) :
-    if len(chain) == 1 : 
-        return chain[0]
-    elif counter == 1 : 
-        #print chain.popleft(), " dies"
-        chain.popleft()
-        return findlast(chain,nth,counter+1)
-    else : 
-        head = chain.popleft()
-        #print head, " survives"
-        chain.append(head)
-        return findlast(chain,nth,1 if counter == nth else counter + 1)
+from collections import deque
+
+def countoff(soldiers, kth, k):
+   i = soldiers.popleft()
+
+   if len(soldiers):
+      if k != kth:
+         k += 1
+         soldiers.append(i)
+      else:
+         k = 1
+
+      return countoff(soldiers, kth, k)
+
+   else:
+      return i
+
+
+def countoffSoldiers(n, kth):
+   soldiers = deque( range(1,n+1) )
+
+   #return countoff(soldiers, kth, 1) # standard Josephus problem
+   return countoff(soldiers, kth, kth)
+
+
+#-----------------------------------------------------------------------------
 
 import time
 def run_iterations(iterations, times) :
-    print '(element recursive)' 
+    print "element recursion" 
     for t in range(times) :
         start = time.time()
         for i in range(iterations) :
-            find()
+            countoffSoldiers(40,3)
         end = time.time()
         print ((end - start) * 1000000 / iterations)
 
-print find()
+print countoffSoldiers(40,3)
 ITER = 1000000
 run_iterations(ITER,10)
+
