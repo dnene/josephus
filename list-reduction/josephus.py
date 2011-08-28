6c14c1a0ba7da2bd09759c7e7d4dbe082d23b027
Dhananjay Nene <dhananjay.nene@gmail.com>
2011-08-29 04:19:15 +0800

    Added contributions by isaac for python

--- list-reduction/josephus.py
+++ list-reduction/josephus.py
@@ -1,18 +1,54 @@
-def shout(chain,nth,counter):
-    while len(chain)>1:
-        chain, counter = [x for n,x in enumerate(chain) if (counter + n) % nth != 0], (n + counter + 1) % nth
-    return chain[0]
+"""
+$ python josephus.py
+26
+list reduction
+17.4075951576
+17.7173249722
+17.6679799557
+17.723637104
+17.6835641861
+17.7069940567
+17.6745419502
+17.7110111713
+17.6747300625
+17.7083261013
+"""
+
+def countoffSoldiers(n,kth):
+   soldiers = list(range(1,n+1))
+
+   #k = kth-1; survivedLastRound = n # standard Josephus problem
+   k = 0; survivedLastRound = n
+
+   while survivedLastRound > 1:
+      survived = 0
+
+      for i in range(survivedLastRound):
+         if i != k: 
+            soldiers[survived] = soldiers[i]
+            survived += 1
+         else:
+            k += kth
+
+      k -= survivedLastRound # wrap around
+      survivedLastRound = survived
+
+   return soldiers[0] 
+
+
+#-----------------------------------------------------------------------------
 
 import time
 def run_iterations(iterations, times) :
-    print '(list reduction)' 
+    print 'list reduction' 
     for t in range(times) :
         start = time.time()
         for i in range(iterations) :
-            shout(range(1,41),3,0)
+            countoffSoldiers(40,3)
         end = time.time()
         print ((end - start) * 1000000 / iterations)
 
-print shout(range(1,41),3,0)
+print countoffSoldiers(40,3)
 ITER = 1000000
 run_iterations(ITER,10)
+
--- oo/josephus.py
+++ oo/josephus.py
@@ -1,54 +1,78 @@
-class Person(object):
-    def __init__(self,count):
-        self.count = count + 1;
-        self.prev = None
+"""
+$ python josephus.py
+26
+object oriented
+105.884628057
+105.946704865
+106.000247002
+105.890681982
+105.9042871
+105.917474985
+105.907362938
+105.803264856
+105.783190012
+105.89677
+"""
 
-        self.next = None
-    def shout(self,shout,nth):
-        if (shout == 1):
-            self.prev.next = self.next
-            self.next.prev = self.prev
-            return 2
-        if shout == nth : return 1
-        else: return shout + 1
-    def __str__(self) :
-        return str(self.count)
+kth = 3; k = 1
 
-class Chain(object):
-    def __init__(self,size):
-        self.first = None
-        last = None
-        for i in range(size):
-            current = Person(i)
-            if self.first == None : self.first = current
-            if last != None :
-                last.next = current
-                current.prev = last
-            last = current
-        self.first.prev = last
-        last.next = self.first
-    def kill(self,nth):
-        current = self.first
-        shout = 1 
-        while current.next != current:
-            shout = current.shout(shout,nth)
-            current = current.next
-        self.first = current
-        return current
+class Soldier(object):
+
+   def __init__(self,previousSoldier,number):
+
+      self.number = number;
+
+      if previousSoldier:
+         self.next = previousSoldier.next
+         previousSoldier.next = self
+         self.previous = previousSoldier
+         self.next.previous = self
+      else:
+         self.next = self
+         self.previous = self
+
+
+   def countoff(self):
+      global k
+      if k == kth:
+         self.previous.next = self.next;
+         self.next.previous = self.previous
+         k = 1
+      else:
+         k += 1
+      return self.next 
+
+
+def countoffSoldiers(n,nth):
+   global kth,k
+   #kth = nth; k = 1 # standard Josephus problem
+   kth = nth; k = nth
+
+   soldier = None
+   for i in (range(1,n+1)):
+      soldier = Soldier(soldier,i)
+
+   soldier = soldier.next
+
+   while soldier.next != soldier:
+      soldier = soldier.countoff()
+
+   return soldier.number
+
+
+#-----------------------------------------------------------------------------
 
 import time
 def run_iterations(iterations, times) :
-    print '(oo)' 
+    print "object oriented" 
     for t in range(times) :
         start = time.time()
         for i in range(iterations) :
-            chain = Chain(40)
-            chain.kill(3)
+            countoffSoldiers(40,3)
         end = time.time()
         print ((end - start) * 1000000 / iterations)
 
-chain = Chain(40)
-chain.kill(3)
-print chain.first
+print countoffSoldiers(40,3)
 ITER = 1000000
 run_iterations(ITER,10)
+
