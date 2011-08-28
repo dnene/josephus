1cfc7ee11abac90a948898f13b2d15971a38752a
Dhananjay Nene <dhananjay.nene@gmail.com>
2011-08-29 01:48:12 +0530

    Contributions by Alex Tkachman for Groovy++

---
+++ contrib/alextkachman/ChainGpp.groovy
@@ -0,0 +1,66 @@
+@Typed package gpptest
+
+class ChainGpp2
+{
+    static class Person
+    {
+        final int count
+        Person prev, next
+
+        Person(int count)
+        {
+            this.count = count+1
+        }
+
+        void remove() {
+            (prev.next = next).prev = prev
+        }
+    }
+
+    Person first
+
+    ChainGpp2(int size)
+    {
+        def last = (first = [0])
+        for (int i = 1 ; i < size ; i++)
+        {
+            last = ((last.next = [i]).prev = last).next
+        }
+        first.prev = last
+        last.next = first
+    }
+
+    void kill(int nth)
+    {
+        def shout = nth
+        def current = first.prev
+        for(; current !== (current = current.next); )
+        {
+            if (shout++ == nth) {
+                current.remove()
+                shout = 1
+            }
+        }
+        first = current
+    }
+}
+
+def runIterations(int iterations, int times) {
+    for (int t = 0 ; t < times ; t++) {
+        System.gc()
+        def start = System.nanoTime()
+        for (int i = 0 ; i < iterations ; i++) {
+            ChainGpp2 chain = [40]
+            chain.kill(3)
+        }
+        long end = System.nanoTime();
+        System.out.println(((end - start) * 1.0 / (iterations * 1000)));
+    }
+}
+
+
+ChainGpp2 chain = [40]
+chain.kill(3)
+println(chain.first.count)
+def ITER = 1000000
+runIterations(ITER,10)
